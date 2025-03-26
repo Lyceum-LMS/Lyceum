caa35d3cc482d6bc262cd9e8e1ffdb1192f2d21c
Harris Popal <hapopal@csumb.edu>
2025-03-25 20:29:43 -0700

    To avoid merge conflicts with other dev branch with complete file, AssignmentsStudentView.js is reverted back to origin/master.

--- src/components/student/AssignmentsStudentView.js
+++ src/components/student/AssignmentsStudentView.js
@@ -1,12 +1,5 @@
-import React, {useEffect, useState} from 'react';
-import { confirmAlert } from 'react-confirm-alert'; // Import
-import 'react-confirm-alert/src/react-confirm-alert.css'; // Import css
-import Button from '@mui/material/Button';
-import AssignmentsView from "../instructor/AssignmentsView";
-import AssignmentAdd from "../instructor/AssignmentAdd";
-import {SERVER_URL} from "../../Constants";
+import React, {useState} from 'react';
 
-// branch fixed
 // student views a list of assignments and assignment grades
 // use the URL  /assignments?studentId= &year= &semester=
 // The REST api returns a list of SectionDTO objects
@@ -16,61 +9,11 @@ import {SERVER_URL} from "../../Constants";
 
 const AssignmentsStudentView = (props) => {
 
-    const headers = ['Course ID', 'Assignment Title', 'Assignment DueDate', 'Score'];
-    const[assignments, setAssignments] = useState('');
-    const[message,setMessage] = useState('');
-    const studentId = 3;
-    const year = new Date().getFullYear();
-    const semester = 'Fall';
-
-    const fetchAssignments = async () => {
-        try{
-            const response = await fetch(`${SERVER_URL}/assignments?studentId=${studentId}&year=${year}&semester=${semester}`);
-            if (response.ok){
-                const data = await response.json();
-                setAssignments(data);
-            } else {
-                const json = await response.json();
-                setMessage("response error: " +json.message);
-            }
-        } catch (err){
-            setMessage("network error " +err);
-        }
-    }
-    useEffect(() => {
-        fetchAssignments();
-    }, []);
 
     return(
-        <div>
-            <h3>Assignments</h3>
-            <h4>{message}</h4>
-            <table className="Center">
-                <thead>
-                <tr>
-                    {headers.map((h,idx) => (<th key={idx}>{h}</th>))}
-                </tr>
-                </thead>
-                <tbody>
-                {assignments.length > 0 ? (
-                    assignments.map((assignment) => (
-                        assignment.assignments.map((item) => (
-                            <tr key={item.id}>
-                                <td>{assignment.courseId}</td>
-                                <td>{item.title}</td>
-                                <td>{item.dueDate}</td>
-                                <td>{item.score}</td>
-                            </tr>
-                        ))
-                    ))
-                ) : (
-                    <tr>
-                        <td colSpan="4">No assignments available</td>
-                    </tr>
-                )}
-                </tbody>
-            </table>
-        </div>
+        <>
+            <h3>Not implemented</h3>
+        </>
     );
 }
 
